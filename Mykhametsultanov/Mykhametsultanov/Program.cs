6012104d885ba1ebdb33f3137cdbdfd3fdf2ea31
WidowZOV <kotyaraobglodannaya@outlook.com>
2025-09-08 15:11:11 +0500

    laba 10 -11

---
+++ Mykhametsultanov/Mykhametsultanov/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 3 стороны треугольника");
+
+double a = Convert.ToDouble(Console.ReadLine());
+
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = Convert.ToDouble(Console.ReadLine());
+
+double p = (a + b + c) / 2;
+
+double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+double R = (a * b * c) / (4 * s);
+
+double S_circle = Math.PI * Math.Pow(R, 2);
+
+Console.WriteLine(S_circle);
