1e6944d47e88695e213e8f28bb14e51509d84602
WidowZOV <kotyaraobglodannaya@outlook.com>
2025-09-08 15:06:38 +0500

    Laba 10 - 11(Karpov)

---
+++ Karpov/Karpov/Program.cs
@@ -0,0 +1,13 @@
+﻿double r = 7.0;
+
+double ugol = 23.0;
+
+double preobrazovanie = ugol * (Math.PI / 180.0);
+
+double x = r * Math.Cos(preobrazovanie);
+
+
+double y = r * Math.Sin(preobrazovanie);
+
+Console.WriteLine($"Величины координаты x равна {x};Величины координаты x равна {x} ");
+
